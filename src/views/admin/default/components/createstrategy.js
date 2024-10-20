371a30f6bf7bb1d10adb795e3821749a232e0b16
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-10-20 16:55:18 +0100

    Update createstrategy.js

--- src/views/admin/default/components/createstrategy.js
+++ src/views/admin/default/components/createstrategy.js
@@ -14,15 +14,75 @@ import {
 } from '@chakra-ui/react';
 
 const CreateStrategyModal = React.memo(({ 
-  isCreateStrategyOpen, 
-  onCreateStrategyClose, 
-  newStrategyName, 
-  hookkey, 
-  tradingViewLink, 
-  handleNameChange, 
-  handleHookKeyChange, 
-  handleSubmit 
+  isCreateStrategyOpen,
+  onCreateStrategyClose,
+  
 }) => {
+const tradingViewLink = `${process.env.REACT_APP_BACKENDAPI}/api/tradingview-webhook`;
+const [newStrategyName, setNewStrategyName] = useState({
+  name: false,
+  hookkey: ""
+  });
+const handleNameChange = (e) => {
+  setNewStrategyName((prev) => ({ ...prev, name: e.target.value }))
+};
+
+const handleHookKeyChange = (e) => {
+  setNewStrategyName((prev) => ({ ...prev, hookkey: e.target.value }))
+};
+
+  const handleSubmit = async() => {
+    const newStrategy = {
+      name: newStrategyName.name,
+      hookkey: newStrategyName.hookkey
+    };
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BACKENDAPI}/api/strategy`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwttoken}`,
+          },
+          body:  JSON.stringify(newStrategy),
+        });
+    
+        if (response.ok) {
+          toast({
+  title: "Strategy created successfully.",
+  status: "success",
+  duration: 5000,
+  isClosable: true,
+});
+          console.log('Strategy added successfully');
+        } else {
+          toast({
+  title: "Error creating strategy.",
+  description: "Please verify the inputs and try again.",
+  status: "error",
+  duration: 5000,
+  isClosable: true,
+});
+          console.error('Error adding strategy');
+        }
+      } catch (error) {
+        toast({
+  title: "Error creating strategy.",
+  description: "Please verify the inputs and try again.",
+  status: "error",
+  duration: 5000,
+  isClosable: true,
+});
+        console.error('Request failed', error);
+      }
+
+     // setStrategies((prevStrategies) => [...prevStrategies, newStrategy]);
+   //  fetchStrategies();
+
+      console.log("newStrategy", newStrategy);
+
+      onCreateStrategyClose();
+  };
+  
   return (
     <Modal isOpen={isCreateStrategyOpen} onClose={onCreateStrategyClose}>
       <ModalOverlay />
@@ -33,18 +93,21 @@ const CreateStrategyModal = React.memo(({
           <FormControl isRequired>
             <FormLabel>Strategy Name</FormLabel>
             <Input 
-              value={newStrategyName} 
+              value={newStrategyName.name} 
               onChange={handleNameChange} 
               placeholder="Enter strategy name" 
             />
+          </FormControl>
 
+        <FormControl isRequired>
             <FormLabel>Webhook Key</FormLabel>
             <Input 
-              value={hookkey} 
+              value={newStrategyName.hookkey} 
               onChange={handleHookKeyChange} 
               placeholder="Enter webhook key" 
             />
-
+       </FormControl>
+        <FormControl>
             <FormLabel mb="4">TradingView Link</FormLabel>
             <Input 
               value={tradingViewLink} 
