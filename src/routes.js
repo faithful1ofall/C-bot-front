137af5c7fd708fb5af80e3b9f5e698975d34c342
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-10-22 06:38:14 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import { Icon } from '@chakra-ui/react';
-import {
-  MdHome,
-  MdLock,
-} from 'react-icons/md';
+import { MdHome, MdLock } from 'react-icons/md';
+import { useParams } from 'react-router-dom';
 
 // Admin Imports
 import MainDashboard from 'views/admin/default';
@@ -15,46 +12,46 @@ import Logger from 'views/admin/default/components/logger';
 // Auth Imports
 import SignInCentered from 'views/auth/signIn';
 
+const EditStrategyWrapper = () => {
+  const { strategyid, selectedPairs } = useParams();
+  return <EditStrategyForm strategyid={strategyid} selectedPairs={selectedPairs} />;
+};
+
 const routes = [
   {
     name: 'Bot Admin interface',
     layout: '/admin',
     path: '/default',
     icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
-    component: <MainDashboard />,
+    element: <MainDashboard />,
   },
   {
     name: 'Edit Strategy',
     layout: '/admin',
     path: '/edit/:strategyid/:selectedPairs',
     icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
-    element: ({ match }) => (
-      <EditStrategyForm
-        strategyid={match.params.strategyid}
-        selectedPairs={match.params.selectedPairs}
-      />
-    )
+    element: <EditStrategyWrapper />
   },
   {
     name: 'Create Strategy',
     layout: '/admin',
     path: '/create',
     icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
-    component: <CreateStrategyModal />,
+    element: <CreateStrategyModal />,
   },
   {
     name: 'Sign In',
     layout: '/auth',
     path: '/sign-in',
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
-    component: <SignInCentered />,
+    element: <SignInCentered />,
   },
   {
     name: 'Logger',
     layout: '/admin',
     path: '/logger',
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
-    component: <Logger />,
+    element: <Logger />,
   },
 ];
 
